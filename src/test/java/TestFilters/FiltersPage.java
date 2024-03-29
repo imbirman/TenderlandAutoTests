9283ae5177e46b3e0224594e2a65717d018049da
Agafonov_German\prog <agafonovgerman@yandex.ru>
2024-03-29 14:39:11 +0300

    Добавлены классы FiltersPage и FiltersTest + базовые переменные и методы. Добавлен тест "Проверка соответствия найденного значения по поиску в фильтре ОКПД" + соответствующие переменные и методы

---
+++ src/test/java/TestFilters/FiltersPage.java
@@ -0,0 +1,86 @@
+package TestFilters;
+
+import TestAuditor.AuditorPage;
+import TestDistributionAutoSearch.DistributionAutoSearchPage;
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+
+import javax.annotation.Nonnull;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class FiltersPage {
+
+    private final SelenideElement logInButton = $x("//div[@id='login-button']"); /** Кнопка входа в систему */
+    private final SelenideElement loginField = $x("//input[@type='text']"); /** Поле для ввода логина */
+    private final SelenideElement passwordField = $x("//input[@type='password']"); /** Поле для ввода пароля */
+    private final SelenideElement confirmLogInButton = $x("//div[@id='landing-popup-login-button']"); /* Кнопка "Войти в систему" */
+
+    /** Поле дерева фильтров */
+    private final SelenideElement filterRoot = $x("//div[@class='dx-sortable tl-filter-content tl-filter-drop-area']");
+    /** Результат поиска внутри фильтра */
+    private final SelenideElement resultSearchInFilter = $x("//span[@class='dx-treelist-search-text']");
+
+    /** Поле поиска внутри фильтра */
+    protected SelenideElement fieldSearchInFilter = $x("//div[(contains(@class,'dx-item dx-multiview-item dx-item-selected'))]//input[@class='dx-texteditor-input']");
+    /** Фильтр "ОКПД 2" в блоке фильтров */
+    protected SelenideElement filterOKPD = $x("//span[text()='ОКПД 2']");
+
+
+    @Step("Ожидание {number}")
+    public FiltersPage waitFor(long number){
+        sleep(number);
+        return new FiltersPage();
+    }
+
+    @Step("Прокрутить до элемента")
+    public FiltersPage scrollToElement(SelenideElement element){
+        element.scrollIntoView(true);
+        return new FiltersPage();
+    }
+
+    @Step("Ввести логин для авторизации")
+    public FiltersPage typeLogin(String login){loginField.sendKeys(login); return new FiltersPage();}
+
+    @Step("Ввести пароль для авторизации")
+    public FiltersPage typePassword(String password){
+        passwordField.sendKeys(password);
+        return new FiltersPage();
+    }
+
+    @Step("Нажать кнопку для открытия окна авторизации")
+    public FiltersPage clickLogInButton(){
+        logInButton.click();
+        return new FiltersPage();
+    }
+
+    @Step("Кликнуть на кнопку \"Войти в систему\"")
+    public FiltersPage clickConfirmLogInButton(){
+        confirmLogInButton.click();
+        return new FiltersPage();
+    }
+
+    @Step("Нажать кнопку {button}")
+    public FiltersPage clickButton(SelenideElement button){
+        button.click();
+        return new FiltersPage();
+    }
+
+    @Step("Перетаскиваем фильтр в поле построения")
+    public FiltersPage dragAndDropFilter(@Nonnull SelenideElement element){
+        actions().clickAndHold(element).moveToElement(filterRoot).release().build().perform();
+        return new FiltersPage();
+    }
+
+    @Step("Ввести значение в поле поиска")
+    public FiltersPage typeSearch(String search){
+        fieldSearchInFilter.sendKeys(search);
+        return new FiltersPage();
+    }
+
+    @Step("Получить текст найденного значения в фильтре \"ОКПД\"")
+    public String getResultSearchByFilter(){
+        return resultSearchInFilter.getText();
+    }
+}
---
+++ src/test/java/TestFilters/FiltersTest.java
@@ -0,0 +1,43 @@
+package TestFilters;
+
+import Base.BaseTest;
+import com.codeborne.selenide.Selenide;
+import io.qameta.allure.Description;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FiltersTest extends BaseTest {
+
+    private final static String BASE_URL = "https://test.v2.tenderland.ru/Home/Landing";
+    private final static String BASE_LOGIN = "AdminTestitTender";
+    private final static String BASE_PASSWORD = "Hyqpmaz0";
+
+    FiltersPage page = new FiltersPage();
+
+    @Description("Открытие сайта для входа")
+    private void openURL(String url){
+        Selenide.open(url);
+    }
+
+    @BeforeEach
+    @Description("Ввод логина/пароля и вход на сайт")
+    public void beforeMethod(){
+        openURL(BASE_URL);
+        page.clickLogInButton()
+                .waitFor(500)
+                .typeLogin(BASE_LOGIN)
+                .waitFor(400)
+                .typePassword(BASE_PASSWORD)
+                .clickConfirmLogInButton();
+    }
+
+    @Test
+    @Description("Проверка соответствия найденного значения по поиску в фильтре ОКПД")
+    public void checkResultSearchFilterOKPD(){
+        assertEquals(page.dragAndDropFilter(page.filterOKPD)
+                .waitFor(500)
+                .typeSearch("(85.11.10.000) Услуги в области дошкольного образования")
+                .getResultSearchByFilter(), "(85.11.10.000) Услуги в области дошкольного образования");
+    }
+}
