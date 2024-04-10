898ca7a5167e8f22fa37305d2798cb5d82059e7e
Agafonov_German\prog <agafonovgerman@yandex.ru>
2024-04-10 14:34:18 +0300

    Добавлены классы MyTendersPage и MyTendersTest + базовые переменные и методы. Добавлен тест "Проверка появления окна подтверждения удаления тендера" + соответствующие переменные и методы

---
+++ src/test/java/TestMyTenders/MyTendersPage.java
@@ -0,0 +1,74 @@
+package TestMyTenders;
+
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.sleep;
+
+import static com.codeborne.selenide.Condition.*;
+
+public class MyTendersPage {
+
+    private final SelenideElement logInButton = $x("//div[@id='login-button']"); /** Кнопка входа в систему */
+    private final SelenideElement loginField = $x("//input[@type='text']"); /** Поле для ввода логина */
+    private final SelenideElement passwordField = $x("//input[@type='password']"); /** Поле для ввода пароля */
+    private final SelenideElement confirmLogInButton = $x("//div[@id='landing-popup-login-button']"); /* Кнопка "Войти в систему" */
+
+    /** Кнопка открытия бокового меню */
+    protected SelenideElement openTabMenu = $x("//i[@class='material-icons-round icon-28px icon-grey md-menu icon-grey-hover common-header-icon']");
+    /** Кнопка в боковом меню "Мои тендеры" */
+    protected SelenideElement buttonTabMenuMyTenders = $x("//div[@id='main-menu-list']//div[text()='Мои тендеры']");
+    /** Кнопка удаления добавленного тендера в списке тендеров */
+    protected SelenideElement buttonDeleteAddedTenderInListTenders = $x("(//div[@class='favourite-kanban-delete-favourite'])[1]");
+
+
+    /** Окно подтверждения удаления */
+    private final SelenideElement windowApproveDelete = $x("//div[@class='dx-overlay-content dx-popup-normal dx-popup-draggable dx-resizable dx-popup-flex-height']");
+
+
+    @Step("Ожидание {number}")
+    public MyTendersPage waitFor(long number){
+        sleep(number);
+        return new MyTendersPage();
+    }
+
+    @Step("Прокрутить до элемента")
+    public MyTendersPage scrollToElement(SelenideElement element){
+        element.scrollIntoView(true);
+        return new MyTendersPage();
+    }
+
+    @Step("Ввести логин для авторизации")
+    public MyTendersPage typeLogin(String login){loginField.sendKeys(login); return new MyTendersPage();}
+
+    @Step("Ввести пароль для авторизации")
+    public MyTendersPage typePassword(String password){
+        passwordField.sendKeys(password);
+        return new MyTendersPage();
+    }
+
+    @Step("Нажать кнопку для открытия окна авторизации")
+    public MyTendersPage clickLogInButton(){
+        logInButton.click();
+        return new MyTendersPage();
+    }
+
+    @Step("Кликнуть на кнопку \"Войти в систему\"")
+    public MyTendersPage clickConfirmLogInButton(){
+        confirmLogInButton.click();
+        return new MyTendersPage();
+    }
+
+    @Step("Нажать кнопку {button}")
+    public MyTendersPage clickButton(SelenideElement button){
+        button.click();
+        return new MyTendersPage();
+    }
+
+    @Step("Проверка появления окна подтверждения удаления")
+    public boolean isCheckVisibleWindowApproveDelete(){
+        return windowApproveDelete.is(visible);
+    }
+}
---
+++ src/test/java/TestMyTenders/MyTendersTest.java
@@ -0,0 +1,44 @@
+package TestMyTenders;
+
+import Base.BaseTest;
+import com.codeborne.selenide.Selenide;
+import io.qameta.allure.Description;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MyTendersTest extends BaseTest {
+
+    private final static String BASE_URL = "https://test.v2.tenderland.ru/Home/Landing";
+    private final static String BASE_LOGIN = "AdminTestitTender";
+    private final static String BASE_PASSWORD = "Hyqpmaz0";
+
+    MyTendersPage page = new MyTendersPage();
+
+    @Description("Открытие сайта для входа")
+    private void openURL(String url){
+        Selenide.open(url);
+    }
+
+    @BeforeEach
+    @Description("Ввод логина/пароля и вход на сайт")
+    public void beforeMethod(){
+        openURL(BASE_URL);
+        page.clickLogInButton()
+                .waitFor(500)
+                .typeLogin(BASE_LOGIN)
+                .waitFor(400)
+                .typePassword(BASE_PASSWORD)
+                .clickConfirmLogInButton()
+                .clickButton(page.openTabMenu)
+                .clickButton(page.buttonTabMenuMyTenders);
+    }
+
+    @Test
+    @Description("Проверка появления окна подтверждения удаления тендера")
+    public void checkWindowApproveDeleteTender(){
+        assertTrue(page.clickButton(page.buttonDeleteAddedTenderInListTenders)
+                .isCheckVisibleWindowApproveDelete());
+    }
+
+}
